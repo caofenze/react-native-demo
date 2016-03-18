b86eff1584dc16e88adc4e3bfc29e06baa87f5fe
caofenze <caofenze@lianjia.com>
2016-03-18 15:23:38 +0800

    canlendar and tabBar

---
+++ calendar/index.ios.js
@@ -0,0 +1,57 @@
+var React = require('react-native');
+var Calendar = require('./calendar');
+
+var{
+  View,
+  AppRegistry,
+  StyleSheet,
+  StatusBarIOS
+} = React;
+
+StatusBarIOS.setHidden(true);
+
+var Index = React.createClass({
+  render: function(){
+    var holiday = {
+      '2015-10-1': '国庆节',
+      '2015-9-10': '教师节',
+      '2016-1-1': '元旦节',
+      '2015-11-11': '双十一'
+    };
+    var check = {
+      '2015-10-1': 'checked',
+      '2015-9-1': 'checked',
+      '2015-7-10': 'checked',
+      '2015-9-10': 'checked'
+    };
+    var headerStyle ={
+      backgroundColor: '#3C9BFD',
+      color:'#fff',
+      fontSize: 15,
+      fontWeight:'500',
+    };
+    return (
+      <View style={styles.container}>
+        <Calendar
+          touchEvent={this.press}
+          headerStyle={headerStyle}
+          holiday={holiday}
+          check={check}
+          />
+      </View>
+    );
+  },
+
+  press: function(str){
+    alert(str);
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'blue'
+  }
+});
+
+AppRegistry.registerComponent('calendar', () => Index);
